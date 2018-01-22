a602684f0dd0346b143d682ac1833938412db7a5
Esterling Accime <esterling@brainbytescreative.com>
2018-01-22 16:50:28 -0500

    Ignoring node_modules folder

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Node files uploader
+
+
---
+++ server.js
@@ -0,0 +1,33 @@
+﻿const Express = require('express');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const app = Express();
+app.use(bodyParser.json());
+const Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+const upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/api/Upload", function (req, res) {
+    upload(req, res, function (err, info) {
+        if (err) {
+            return res.json("Something went wrong!");
+        }
+        console.log("Info: ", req.files);
+        return res.json("File uploaded sucessfully!.");
+    });
+});
+
+app.listen(2000, function () {
+    console.log("Listening to port 2000");
+});
